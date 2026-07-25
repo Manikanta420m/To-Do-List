a9987943cb4abd7c859bda0e8246c1f7c8114da8
Manikanta420m <manikanta420m@gmail.com>
2026-07-25 09:06:20 +0530

    "Issues Resolved"

--- src/to-do-list.jsx
+++ src/to-do-list.jsx
@@ -4,20 +4,37 @@ function ToDoList(){
    const [tasks,setTasks]=useState([]);
    const [newTasks,setNewTask]=useState("");
 
-   function inputChange(){
-
+   function inputChange(e){
+       setNewTask(e.target.value);
    }
 
-   function addtask(){
-
-   }
+    function addtask() {
+        if (newTasks.trim() !== "") {
+            setTasks(t => [...t, newTasks]);
+            setNewTask("");
+        }
+    }
 
    function removeTask(idx){
-
+       const updatedTasks=tasks.filter((_,i)=>i!=idx);
+       setTasks(updatedTasks);
    }
 
-   function moveUp(){
-
+   function moveUp(idx){
+    if(idx>0){
+       const updatedTasks=[...tasks];
+       [updatedTasks[idx],updatedTasks[idx-1]]=
+       [updatedTasks[idx-1],updatedTasks[idx]]
+       setTasks(updatedTasks);
+    }
+   }
+   function moveDown(idx){
+    if(idx<tasks.length-1){
+       const updatedTasks=[...tasks];
+       [updatedTasks[idx],updatedTasks[idx+1]]=
+       [updatedTasks[idx+1],updatedTasks[idx]]
+       setTasks(updatedTasks);
+    }
    }
 
    return(
@@ -27,8 +44,24 @@ function ToDoList(){
                 <input
                     type="text"
                     placeholder="Enter a Task...."
+                    value={newTasks}
+                    onChange={inputChange}
                 />
+                <button className="add-button"
+                        onClick={addtask}>
+                    Add
+                </button>
             </div>
+            <ol>
+                {tasks.map((task,idx)=>
+                   <li key={idx}>
+                    <span className="text">{task}</span>
+                    <button className="delButton" onClick={()=>removeTask(idx)}>Delete</button>
+                    <button className="moveUp" onClick={()=>moveUp(idx)}>👆</button>
+                    <button className="moveDown" onClick={()=>moveDown(idx)}>👇</button>
+                   </li>
+                )}
+            </ol>
         </div>
    );
 }
